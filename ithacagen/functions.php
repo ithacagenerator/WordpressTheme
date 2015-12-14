faf895d0e7a5aea0ecdb0302d1ea10e5dc72cd13
Tyler <tylerfinck@gmail.com>
2015-12-14 09:54:38 -0500

    removed emoji smiley inserted by wordpress, sorry wordpress. also updated some styles, in the name of usability.

--- ithacagen/functions.php
+++ ithacagen/functions.php
@@ -169,3 +169,8 @@ add_action( 'init', 'my_add_excerpts_to_pages' );
 function my_add_excerpts_to_pages() {
      add_post_type_support( 'page', 'excerpt' );
 }
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
