4384301aec48716f2b07e35be4735f65505193c0
Tyler <tylerfinck@gmail.com>
2015-12-14 10:35:30 -0500

    final fixes for the day

--- ithacagen/404.php
+++ ithacagen/404.php
@@ -20,7 +20,6 @@ get_header(); ?>
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'ithacagen' ); ?></p>
 
-					<?php get_search_form(); ?>
 
 					
 
